44e17677554ab0b318e02829399720557294cce4
adsfryt <93540011+adsfryt@users.noreply.github.com>
2025-01-10 23:52:34 +0300

    Update data.js

--- new/data.js
+++ new/data.js
@@ -7,19 +7,19 @@ class Data{
     ADDRESS_SITE="http://localhost:3000"
     ADDRESS_SERVER="http://localhost:5000"
     WS_ADDRESS_SERVER="ws://localhost:5000"
-    AccessToken = "sdfuyintwefcxbtixdfggjhxcerf";
-    RefreshToken = "wktnaowereiuthqe-egweviawioeuf";
-    Yandex_client_id = "44c728c0ab8f455784e845387e2c9bd8";
-    Github_client_id = "Ov23liKITzR2Uv3VpUis";
-    Github_secret = "0ec1bf07a584b55adaaeec763550606d293df410";
-    Yandex_secret = "ffde646e2a4d4f1fb830ea1de2bf1f6a";
+    AccessToken = "";
+    RefreshToken = "";
+    Yandex_client_id = "";
+    Github_client_id = ";
+    Github_secret = "";
+    Yandex_secret = "";
 
     //TG
-    Yandex_client_id_tg = "7ad05c24919c421e9c59974d8b7e2a28";
-    Yandex_secret_tg = "6beb28762a03406e847d00a23aec6df1";
+    Yandex_client_id_tg = "";
+    Yandex_secret_tg = "";
 
-    Github_client_id_tg = "Ov23lizVCHL6TuYqCqm1";
-    Github_secret_tg = "46e1d0df21818d702b11d05ddae1b640c2e3d480";
+    Github_client_id_tg = "";
+    Github_secret_tg = "";
 
 
 }
